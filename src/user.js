e59aa572bccaec4e8394e742486743e8861cfc9e
Dmitry Sakhonchik <dmitry.sakhonchik@altezza.org>
2024-12-19 17:09:39 +0100

    feat: fix solution

--- src/user.js
+++ src/user.js
@@ -108,7 +108,7 @@ module.exports.createUsersSortFn = function (TestUtils) {
  */
 module.exports.celebrate = function (users) {
   for (let i = 0; i < users.length; i++) {
-    if (i % 2 !== 0) {
+    if (i % 2 === 0) {
       users[i].celebrateBirthday();
     }
   }
